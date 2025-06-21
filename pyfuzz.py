f65176564f4f0e8987eff161e46897c71ecd6eea
Omar-Arabi1 <omarbuisness7@gamil.com>
2025-06-21 17:38:12 +0300

    modularized a bit of it

--- pyfuzz.py
+++ pyfuzz.py
@@ -3,23 +3,22 @@ import typer
 
 pyfuzz = typer.Typer()
 
+def ishidden(check_hidden: str, showhidden):
+    if showhidden:
+        print(check_hidden)
+    else:
+        if not check_hidden.startswith("."):
+            print(check_hidden)
+
 def list_dir(list_to_loop, part_of_name, showhidden):
     for i in list_to_loop:
         if "_" in i:
             file_split = i.split("_")              
             if part_of_name in file_split[0].lower():
-                if showhidden:  
-                    print(i)
-                else:
-                    if not i.startswith("."):
-                        print(i)          
+                ishidden(i, showhidden)
         else:
             if part_of_name in i.lower():
-                if showhidden:
-                    print(i)
-                else:
-                    if not i.startswith("."):
-                        print(i)
+                ishidden(i, showhidden)
 
 @pyfuzz.command()
 def search(file_name: str, current_dir: str = os.getcwd(), folders: bool = False, both: bool = False, show_hidden: bool = False):
@@ -32,13 +31,12 @@ def search(file_name: str, current_dir: str = os.getcwd(), folders: bool = False
     all = []
     
     for item in os.listdir(current_dir):
-        if os.path.isfile(item) and not both:
-            files.append(item)
-        elif os.path.isdir(item) and not both:
-            directories.append(item)
-        elif both:
+        if both:
             all.append(item)
-    
+        elif os.path.isfile(item):
+            files.append(item)
+        elif os.path.isdir(item):
+            directories.append(item)
 
     if not folders and not both:
         list_dir(files, file_name, show_hidden)
